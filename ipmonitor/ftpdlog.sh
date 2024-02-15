7956cff6d702cf3cc49c69d9ba8cb0e2bcffd5cb
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-02-15 18:31:07 +0530

    Update ftpdlog.sh

--- ipmonitor/ftpdlog.sh
+++ ipmonitor/ftpdlog.sh
@@ -3,7 +3,7 @@
 source /home/sample/scripts/dataset.sh
 
 function ftpd_log() {
-	cat /var/log/messages | grep -ie "$(if (($(date -d '1 hour ago' +"%-d") < 10)); then date -d '1 hour ago' +"%b  %-d %H:"; else date -d '1 hour ago' +"%b %d %H:"; fi)" | grep "pure-ftpd:" | grep "Authentication failed for user" | awk '{print $1,$2,$6,$NF}' | sed 's/(?@//;s/)//;s/[][]//g' | awk '{printf "%-15s %-22s %-50s\n","DATE: "$1" "$2,"IP: "$3,"USER: "$NF}' | sort | uniq -c | sort -k8 >>$temp/ftpdlog_$time.txt
+	cat /var/log/messages | grep -ie "$(if (($(date -d '1 hour ago' +"%-d") < 10)); then date -d '1 hour ago' +"%b  %-d %H:"; else date -d '1 hour ago' +"%b %d %H:"; fi)" | grep "pure-ftpd:" | grep "Authentication failed for user" | awk '{print $1,$2,$3,$6,$NF}' | sed 's/(?@//;s/)//;s/[][]//g' | awk '{printf "%-15s %-17s %-22s %-50s\n","DATE: "$1" "$2,"TIME: "$3,"IP: "$4,"USER: "$NF}' | sort | uniq -c >>$temp/ftpdlog_$time.txt
 }
 
 function static_ip() {
@@ -11,7 +11,7 @@ function static_ip() {
 		staticip=($(cat $scripts/ipmonitor/staticip.txt))
 		scount=${#staticip[@]}
 
-		iplist=$(cat $temp/ftpdlog_$time.txt | awk '{print $6}' | sort | uniq)
+		iplist=$(cat $temp/ftpdlog_$time.txt | awk '{print $8}' | sort | uniq)
 
 		for ((i = 0; i < scount; i++)); do
 			iplist=$(echo "$iplist" | grep -v "${staticip[i]}")
@@ -34,7 +34,7 @@ function check_log() {
 			whois=$(sh $scripts/ipmonitor/iplookup.sh ${ips[i]})
 
 			while IFS= read -r line; do
-				printf "%-90s %-10s\n" "$line" "ID: $whois" >>$temp/failed-ftpd_$time.txt
+				printf "%-110s %-10s\n" "$line" "ID: $whois" >>$temp/failed-ftpd_$time.txt
 			done <<<"$data"
 		fi
 	done
@@ -42,7 +42,7 @@ function check_log() {
 
 function sort_log() {
 	if [ -r $temp/failed-ftpd_$time.txt ] && [ -s $temp/failed-ftpd_$time.txt ]; then
-		sortlog=$(cat $temp/failed-ftpd_$time.txt | awk '{if($10!="") print}' | sort -k8)
+		sortlog=$(cat $temp/failed-ftpd_$time.txt | awk '{for (i=0;i<NF;i++) {if($i=="ID:" && $(i+1)!="") print}}' | sort -k8)
 
 		if [[ ! -z $sortlog ]]; then
 			echo "$sortlog" >>$svrlogs/cphulk/iplist/failed-ftpd_$time.txt
@@ -62,14 +62,14 @@ function summary() {
 	if [ ! -z $ftpdlog ]; then
 		count=$(wc -l $ftpdlog | awk '{print $1}')
 
-		uniqipcount=$(cat $ftpdlog | awk '{print $6}' | sort | uniq | wc -l)
+		uniqipcount=$(cat $ftpdlog | awk '{print $8}' | sort | uniq | wc -l)
 
 		failedftpd=($(find $temp -type f -name "failed-ftpd*" -exec ls -lat {} + | grep "$(date +"%F_%H:")" | head -1 | awk '{print $NF}'))
 
 		if [ ! -z $failedftpd ]; then
 			newcount=$(wc -l $failedftpd | awk '{print $1}')
 
-			newuniqip=$(cat $failedftpd | awk '{print $6}' | sort | uniq | wc -l)
+			newuniqip=$(cat $failedftpd | awk '{print $8}' | sort | uniq | wc -l)
 
 			blacklist=($(find $svrlogs/cphulk/block -type f -name "ftpdip-blacklisted*" -exec ls -lat {} + | grep "$(date +"%F_%H:")" | head -1 | awk '{print $NF}'))
 
